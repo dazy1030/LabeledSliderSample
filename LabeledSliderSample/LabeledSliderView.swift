0340621d043dd44e315449352f605744892630a2
dazy1030 <dev.nodajima@gmail.com>
2020-10-24 17:39:08 +0900

    feat: LabeledSliderView追加

---
+++ LabeledSliderSample/LabeledSliderView.swift
@@ -0,0 +1,20 @@
+//
+//  LabeledSliderView.swift
+//  LabeledSliderSample
+//
+//  Created by Naoki Odajima on 2020/10/24.
+//
+
+import UIKit
+
+class LabeledSliderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
